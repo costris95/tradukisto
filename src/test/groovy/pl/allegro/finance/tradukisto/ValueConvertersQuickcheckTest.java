52c7d7f975ff799829d698375cafb8bb77f27df0
Alvaro Balirac <abalirac@gmail.com>
2017-12-20 16:48:23 +0100

    añadidos tests de quickcheck de todos los idiomas

--- src/test/groovy/pl/allegro/finance/tradukisto/ValueConvertersQuickcheckTest.java
+++ src/test/groovy/pl/allegro/finance/tradukisto/ValueConvertersQuickcheckTest.java
@@ -1,14 +1,14 @@
 package pl.allegro.finance.tradukisto;
 
-
-//import static net.java.quickcheck.generator.PrimitiveGenerators.integers;
 import static net.java.quickcheck.generator.PrimitiveGeneratorsIterables.someIntegers;
-//import net.java.quickcheck.Generator;
-//import net.java.quickcheck.characteristic.Classification;
-//import net.java.quickcheck.collection.Pair;
-//import net.java.quickcheck.generator.iterable.Iterables;
 import static org.junit.Assert.*;
 import static pl.allegro.finance.tradukisto.ValueConverters.BRAZILIAN_PORTUGUESE_INTEGER;
+import static pl.allegro.finance.tradukisto.ValueConverters.ENGLISH_INTEGER;
+import static pl.allegro.finance.tradukisto.ValueConverters.GERMAN_INTEGER;
+import static pl.allegro.finance.tradukisto.ValueConverters.POLISH_INTEGER;
+import static pl.allegro.finance.tradukisto.ValueConverters.RUSSIAN_INTEGER;
+import static pl.allegro.finance.tradukisto.ValueConverters.CZECH_INTEGER;
+import static pl.allegro.finance.tradukisto.ValueConverters.SPANISH_INTEGER;
 
 import org.junit.Test;
 
@@ -22,28 +22,208 @@ public class ValueConvertersQuickcheckTest {
     @Test
     public void convertNumbersBrazilianEmpty() {
 
-        for (Integer anyNumber : someIntegers(0, 10000000)) {
+        for (Integer anyNumber : someIntegers(0, 1000000000)) {
             String words = BRAZILIAN_PORTUGUESE_INTEGER.asWords(anyNumber);
             //System.out.println(words + "\n");
             assertFalse(words.isEmpty());
         }
     }
-    
+
     @Test
     public void convertNumbersBrazilianContainsNumbers() {
 
-        for (Integer anyNumber : someIntegers(0, 10000000)) {
+        for (Integer anyNumber : someIntegers(0, 1000000000)) {
             String words = BRAZILIAN_PORTUGUESE_INTEGER.asWords(anyNumber);
             //System.out.println(words + "\n");
             assertFalse(words.matches(".*\\d+.*"));
         }
     }
-    
+
     @Test
     public void convertNumbersBrazilianContainsWeirdChars() {
 
-        for (Integer anyNumber : someIntegers(0, 10000000)) {
+        for (Integer anyNumber : someIntegers(0, 1000000000)) {
             String words = BRAZILIAN_PORTUGUESE_INTEGER.asWords(anyNumber);
+            //System.out.println(words + "\n");
+            assertFalse(words.matches(".*[ρη].*"));
+        }
+    }
+
+    @Test
+    public void convertNumbersEnglishEmpty() {
+
+        for (Integer anyNumber : someIntegers(0, 1000000000)) {
+            String words = ENGLISH_INTEGER.asWords(anyNumber);
+            //System.out.println(words + "\n");
+            assertFalse(words.isEmpty());
+        }
+    }
+
+    @Test
+    public void convertNumbersEnglishContainsNumbers() {
+
+        for (Integer anyNumber : someIntegers(0, 1000000000)) {
+            String words = ENGLISH_INTEGER.asWords(anyNumber);
+            //System.out.println(words + "\n");
+            assertFalse(words.matches(".*\\d+.*"));
+        }
+    }
+
+    @Test
+    public void convertNumbersEnglishContainsWeirdChars() {
+
+        for (Integer anyNumber : someIntegers(0, 1000000000)) {
+            String words = ENGLISH_INTEGER.asWords(anyNumber);
+            //System.out.println(words + "\n");
+            assertFalse(words.matches(".*[ρη].*"));
+        }
+    }
+
+    @Test
+    public void convertNumbersGermanEmpty() {
+
+        for (Integer anyNumber : someIntegers(0, 1000000000)) {
+            String words = GERMAN_INTEGER.asWords(anyNumber);
+            //System.out.println(words + "\n");
+            assertFalse(words.isEmpty());
+        }
+    }
+
+    @Test
+    public void convertNumbersGermanContainsNumbers() {
+
+        for (Integer anyNumber : someIntegers(0, 1000000000)) {
+            String words = GERMAN_INTEGER.asWords(anyNumber);
+            //System.out.println(words + "\n");
+            assertFalse(words.matches(".*\\d+.*"));
+        }
+    }
+
+    @Test
+    public void convertNumbersGermanContainsWeirdChars() {
+
+        for (Integer anyNumber : someIntegers(0, 1000000000)) {
+            String words = GERMAN_INTEGER.asWords(anyNumber);
+            //System.out.println(words + "\n");
+            assertFalse(words.matches(".*[ρη].*"));
+        }
+    }
+
+    @Test
+    public void convertNumbersPolishEmpty() {
+
+        for (Integer anyNumber : someIntegers(0, 1000000000)) {
+            String words = POLISH_INTEGER.asWords(anyNumber);
+            //System.out.println(words + "\n");
+            assertFalse(words.isEmpty());
+        }
+    }
+
+    @Test
+    public void convertNumbersPolishContainsNumbers() {
+
+        for (Integer anyNumber : someIntegers(0, 1000000000)) {
+            String words = POLISH_INTEGER.asWords(anyNumber);
+            //System.out.println(words + "\n");
+            assertFalse(words.matches(".*\\d+.*"));
+        }
+    }
+
+    @Test
+    public void convertNumbersPolishContainsWeirdChars() {
+
+        for (Integer anyNumber : someIntegers(0, 1000000000)) {
+            String words = POLISH_INTEGER.asWords(anyNumber);
+            //System.out.println(words + "\n");
+            assertFalse(words.matches(".*[ρη].*"));
+        }
+    }
+
+    @Test
+    public void convertNumbersRussianEmpty() {
+
+        for (Integer anyNumber : someIntegers(0, 1000000000)) {
+            String words = RUSSIAN_INTEGER.asWords(anyNumber);
+            //System.out.println(words + "\n");
+            assertFalse(words.isEmpty());
+        }
+    }
+
+    @Test
+    public void convertNumbersRussianContainsNumbers() {
+
+        for (Integer anyNumber : someIntegers(0, 1000000000)) {
+            String words = RUSSIAN_INTEGER.asWords(anyNumber);
+            //System.out.println(words + "\n");
+            assertFalse(words.matches(".*\\d+.*"));
+        }
+    }
+
+    @Test
+    public void convertNumbersRussianContainsWeirdChars() {
+
+        for (Integer anyNumber : someIntegers(0, 1000000000)) {
+            String words = RUSSIAN_INTEGER.asWords(anyNumber);
+            //System.out.println(words + "\n");
+            assertFalse(words.matches(".*[ρη].*"));
+        }
+    }
+
+    @Test
+    public void convertNumbersCzechEmpty() {
+
+        for (Integer anyNumber : someIntegers(0, 1000000000)) {
+            String words = CZECH_INTEGER.asWords(anyNumber);
+            //System.out.println(words + "\n");
+            assertFalse(words.isEmpty());
+        }
+    }
+
+    @Test
+    public void convertNumbersCzechContainsNumbers() {
+
+        for (Integer anyNumber : someIntegers(0, 1000000000)) {
+            String words = CZECH_INTEGER.asWords(anyNumber);
+            //System.out.println(words + "\n");
+            assertFalse(words.matches(".*\\d+.*"));
+        }
+    }
+
+    @Test
+    public void convertNumbersCzechContainsWeirdChars() {
+
+        for (Integer anyNumber : someIntegers(0, 1000000000)) {
+            String words = CZECH_INTEGER.asWords(anyNumber);
+            //System.out.println(words + "\n");
+            assertFalse(words.matches(".*[ρη].*"));
+        }
+    }
+
+    @Test
+    public void convertNumbersSpanishEmpty() {
+
+        for (Integer anyNumber : someIntegers(0, 1000000000)) {
+            String words = SPANISH_INTEGER.asWords(anyNumber);
+            //System.out.println(words + "\n");
+            assertFalse(words.isEmpty());
+        }
+    }
+
+    @Test
+    public void convertNumbersSpanishContainsNumbers() {
+
+        for (Integer anyNumber : someIntegers(0, 1000000000)) {
+            String words = SPANISH_INTEGER.asWords(anyNumber);
+            //System.out.println(words + "\n");
+            assertFalse(words.matches(".*\\d+.*"));
+        }
+    }
+
+    @Test
+    public void convertNumbersSpanishContainsWeirdChars() {
+
+        for (Integer anyNumber : someIntegers(0, 1000000000)) {
+            String words = SPANISH_INTEGER.asWords(anyNumber);
             //System.out.println(words + "\n");
             assertFalse(words.matches(".*[ρη].*"));
         }
